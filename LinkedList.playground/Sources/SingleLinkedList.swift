45c1ad2c9f6674a04e5b7e43f2c0f892d515fe69
ankit bharti <ankitbharti1994@gmail.com>
2020-01-12 13:49:50 +0530

    written the documentation

--- LinkedList.playground/Sources/SingleLinkedList.swift
+++ LinkedList.playground/Sources/SingleLinkedList.swift
@@ -1,20 +1,30 @@
 import Foundation
 
+/// Create and manage the sequential list of the same type using linked list
 public struct SingleLinkedList<T: Equatable>: LinkedList, IteratorProtocol {
     public typealias Element = T
+    
+    /// head of a singly linked list
     public var head: Node<T>? = nil
+    
+    /// counter for iterator protocol
     private var counter = 0
     
+    /// empty init method
     public init() {}
     
+    /// init with head value
+    /// - Parameter value: value which contains the head value
     public init(value: Element) {
         self.head = Node(value: value)
     }
-
+    
+    /// check if linked list is empty
     public var isEmpty: Bool {
         head == nil
     }
-
+    
+    /// find the total number of element in linked list
     public var count: Int {
         var localHead = head
         var nums = 0
@@ -31,6 +41,8 @@ public struct SingleLinkedList<T: Equatable>: LinkedList, IteratorProtocol {
         return nums
     }
     
+    /// find a particular value inside linked list
+    /// - Parameter value: searcable value
     public func isAvailable(_ value: T) -> Bool {
         var isValueExist = false
         var localHead = head
@@ -52,7 +64,9 @@ public struct SingleLinkedList<T: Equatable>: LinkedList, IteratorProtocol {
     
         return isValueExist
     }
-
+    
+    /// append new value to the end of the linked list
+    /// - Parameter value: value to append
     public mutating func append(_ value: T) {
         let node = Node(value: value)
         
@@ -74,6 +88,10 @@ public struct SingleLinkedList<T: Equatable>: LinkedList, IteratorProtocol {
         }
     }
     
+    /// insert new value at a given position
+    /// - Parameters:
+    ///   - value: value which want to insert into linked list
+    ///   - index: index at which want to insert new value
     public mutating func insert(_ value: T, at index: Int) {
         guard count >= index else {
             fatalError("Index \(index) is out of bounds.")
@@ -100,6 +118,8 @@ public struct SingleLinkedList<T: Equatable>: LinkedList, IteratorProtocol {
         }
     }
     
+    /// insert new value to the start of the linked list
+    /// - Parameter value: value, which want to insert
     public mutating func insertLeft(_ value: T) {
         let node = Node(value: value)
         let _head = head
@@ -107,10 +127,15 @@ public struct SingleLinkedList<T: Equatable>: LinkedList, IteratorProtocol {
         head?.next = _head
     }
     
+    /// append value to the end of the linked list
+    /// - Parameter value: value, which want to insert.
     public mutating func insertRight(_ value: T) {
         self.append(value)
     }
     
+    /// find the node at a given position
+    /// - Parameter index: index for the searching item
+    /// - Returns: node at given position
     public func node(at index: Int) -> Node<T>? {
         guard index < count else { return nil }
         
